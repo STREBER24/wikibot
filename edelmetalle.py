1a12b2a24f0b77c4d8aa89a498fa15fd76b9a1e2
STREBER24 <largot@gmx.de>
2023-11-05 15:06:50 +0100

    add platinum and palladium

--- edelmetalle.py
+++ edelmetalle.py
@@ -10,31 +10,36 @@ def fetch(metall: str):
     data = res.json()
     return data[-1]['d'], data[-1]['v'][0]
 
-def fetchGoldSilver():
-    silber = fetch('silver')
-    gold = fetch('gold_pm')
-    assert silber[0] == gold[0]
-    datum = utils.formatDate(silber[0][8:10], silber[0][5:7], silber[0][0:4])
-    return datum, str(gold[1]), str(silber[1])
+def fetchTwo(names: tuple[str, str]):
+    data_a = fetch(names[0])
+    data_b = fetch(names[1])
+    assert data_a[0] == data_b[0]
+    datum = utils.formatDate(data_a[0][8:10], data_a[0][5:7], data_a[0][0:4])
+    return datum, (str(data_a[1]), str(data_b[1]))
 
-def run():
+def update(template: str, apiNames: tuple[str, str], displayNames: tuple[str, str], option: str):
     site = pywikibot.Site('de', 'wikipedia')
-    page = pywikibot.Page(site, 'Vorlage:Goldpreis')
+    page = pywikibot.Page(site, f'Vorlage:{template}')
+    print(page)
     daysSinceLastRevistion = (time.time() - page.latest_revision['timestamp'].timestamp())/60/60/24
     if daysSinceLastRevistion < 7:
         print('skip update')
         return False
-    if not(page.latest_revision['user'] == 'DerIchBot' or input(f'{page.latest_revision["user"]} hat die Seite Vorlage:Goldpreis zuletzt geändert. Trotzdem fortfahren? ').strip().lower() in ['y', 'j', 'ja', 'yes']):
+    if not(page.latest_revision['user'] == 'DerIchBot' or input(f'{page.latest_revision["user"]} hat die Seite Vorlage:{template} zuletzt geändert. Trotzdem fortfahren? ').strip().lower() in ['y', 'j', 'ja', 'yes']):
         return False
     noinclude = wtp.parse(page.text).get_tags()[1]
     assert noinclude.name == 'noinclude'
-    datum, gold, silber = fetchGoldSilver()
-    page.text = "<includeonly>{{#if:{{{Datum|}}} | " + datum + " | {{#if:{{{AG|}}}\n| {{formatnum:\n     {{#expr: ( " + silber + " <!-- Silber in USD --> / {{Wechselkursdaten|USD}} * {{Wechselkursdaten|{{{1|USD}}}}} / {{#if:{{{Gramm|}}}|31.1034768|1}} ) ^ {{#if:{{{Invert|}}}|-1|1}} * {{{Faktor|1}}} round {{{NKS|2}}} }}\n  }}\n| {{formatnum:\n     {{#expr: ( " + gold + " <!-- Gold in USD --> / {{Wechselkursdaten|USD}} * {{Wechselkursdaten|{{{1|USD}}}}} / {{#if:{{{Gramm|}}}|31.1034768|1}} ) ^ {{#if:{{{Invert|}}}|-1|1}} * {{{Faktor|1}}} round {{{NKS|2}}} }}\n  }}\n}}\n}}</includeonly>" + str(noinclude)
+    datum, data = fetchTwo(apiNames)
+    page.text = "<includeonly>{{#if:{{{Datum|}}} | " + datum + " | {{#if:{{{" + option + "|}}}\n| {{formatnum:\n     {{#expr: ( " + data[0] + " <!-- " + displayNames[0] + " in USD --> / {{Wechselkursdaten|USD}} * {{Wechselkursdaten|{{{1|USD}}}}} / {{#if:{{{Gramm|}}}|31.1034768|1}} ) ^ {{#if:{{{Invert|}}}|-1|1}} * {{{Faktor|1}}} round {{{NKS|2}}} }}\n  }}\n| {{formatnum:\n     {{#expr: ( " + data[1] + " <!-- " + displayNames[1] + " in USD --> / {{Wechselkursdaten|USD}} * {{Wechselkursdaten|{{{1|USD}}}}} / {{#if:{{{Gramm|}}}|31.1034768|1}} ) ^ {{#if:{{{Invert|}}}|-1|1}} * {{{Faktor|1}}} round {{{NKS|2}}} }}\n  }}\n}}\n}}</includeonly>" + str(noinclude)
     site.login()
     assert site.logged_in()
     page.save(botflag=True, minor=False, summary=(f'Bot: Aktualisiere Preise: Schlusskurs vom {datum}'))
     site.logout()
     return True
+    
+def run():
+    update('Goldpreis', ('silver', 'gold_pm'), ('Silber', 'Gold'), 'AG')
+    update('Platinpreis', ('palladium_pm', 'platinum_pm'), ('Palladium ', 'Platin'), 'PD')
 
 if __name__ == '__main__':
     run()
