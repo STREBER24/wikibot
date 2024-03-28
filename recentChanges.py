8e5cd4ce50f9cc36ccee3b538bdba1df9d6615e5
STREBER24 <largot@gmx.de>
2024-03-29 00:05:29 +0100

    add code to monitor recent changes and search for weird usage of Vorlage:Internetquelle

---
+++ recentChanges.py
@@ -0,0 +1,187 @@
+from pywikibot.comms import eventstreams
+from datetime import datetime
+import wikitextparser as wtp
+from typing import Literal
+import pywikibot
+import utils
+import json
+import re
+import io
+
+def parseWeirdDateFormats(date: str|None):
+    ''' Wandelt möglichts alle Datumsformate, die die Vorlage Internetquelle akzeptiert in Format YYYY-MM-DD um '''
+    try:
+        if date == None: return None
+        if re.match('^[0-9]{4}(-[0-9]{2}(-[0-9]{2})?)?$', date): 
+            return date
+        if re.match('^[0-9]{4}-[0-9]{2}-[0-9]$', date): 
+            return date.split('-')[0] + '-' + date.split('-')[1] + '-' + date.split('-')[2].rjust(2,'0')
+        if re.match('^[0-9][0-9]?.[0-9][0-9]?.[0-9]{4}$', date): 
+            return date.split('.')[2] + '-' + date.split('.')[1].rjust(2,'0') + '-' + date.split('.')[0].rjust(2,'0')
+        if re.match('^[0-9][0-9]?. (Januar|Jänner|Februar|März|April|Mai|Juni|Juli|August|September|Oktober|November|Dezember) [0-9]{4}$', date):
+            return date.split(' ')[2] + '-' + str({'Januar':1, 'Jänner':1,'Februar':2,'März':3,'April':4,'Mai':5,'Juni':6,'Juli':7,'August':8,'September':9,'Oktober':10,'November':11,'Dezember':12}[date.split(' ')[1]]).rjust(2,'0') + '-' + date.split(' ')[0][:-1].rjust(2,'0')
+        if re.match('^[0-9][0-9]? (January|February|March|April|May|June|July|August|September|October|November|December) [0-9]{4}$', date):
+            return date.split(' ')[2] + '-' + str(['January','February','March','April','May','June','July','August','September','October','November','December'].index(date.split(' ')[1])+1).rjust(2,'0') + '-' + date.split(' ')[0].rjust(2,'0')
+        return None
+    except Exception as e:
+        e.add_note(f'failed while parsing weird date "{date}"')
+        raise e
+
+def getTodayString():
+    return datetime.now().strftime('%Y-%m-%d')
+
+def datesOk(template: wtp.Template) -> Literal[True] | str:
+    ''' Prüft, ob Daten der Vorlagen Internetquelle und Literatur in der Zukunft liegen '''
+    if template.name.strip() not in ['Internetquelle', 'Literatur']:
+        return True
+    todayString = getTodayString()
+    isInternetquelle = template.name.strip() == 'Internetquelle'
+    abruf   = parseWeirdDateFormats(utils.findTemplateArg(template, 'abruf'  if isInternetquelle else 'Abruf'))
+    zugriff = parseWeirdDateFormats(utils.findTemplateArg(template, 'zugriff'if isInternetquelle else 'Zugriff'))
+    datum   = parseWeirdDateFormats(utils.findTemplateArg(template, 'datum'  if isInternetquelle else 'Datum'))
+    if abruf == None and zugriff == None and isInternetquelle: return "Pflichtparameter abruf nicht gesetzt."
+    if abruf == None and zugriff == None and not isInternetquelle: return True
+    if abruf != None and zugriff != None: return "Parameter abruf und zugriff beide gesetzt."
+    if abruf == None: abruf = zugriff
+    if abruf > todayString: return "Parameter abruf/zugriff liegt in der Zukunft."
+    if datum != None and datum > todayString: return "Parameter datum liegt in der Zukunft."
+    return True
+
+def archiveParamsOk(template: wtp.Template) -> Literal[True] | str:
+    ''' Prüft, ob Parameter archiv-url und archiv-datum der Vorlage Internetquelle konsistent sind '''
+    if template.name.strip() != 'Internetquelle': return True
+    if utils.findTemplateArg(template, 'titel') == None: return 'Pflichtparameter titel nicht gesetzt.'
+    archivurl   = utils.findTemplateArg(template, 'archiv-url')
+    archivdatum = parseWeirdDateFormats(utils.findTemplateArg(template, 'archiv-datum'))
+    abrufdatum  = parseWeirdDateFormats(utils.findTemplateArg(template, 'abruf'))
+    datum       = parseWeirdDateFormats(utils.findTemplateArg(template, 'datum'))
+    if archivurl == None and archivdatum == None: return True
+    if archivurl == None and archivdatum != None: return "Parameter archiv-datum ohne Parameter archiv-url gesetzt."
+    if archivdatum != None: archivdatum = archivdatum.replace('-', '')
+    if abrufdatum  != None: abrufdatum  = abrufdatum.replace('-', '')
+    if datum       != None: datum       = datum.replace('-', '')
+    if not re.match('^https://web.archive.org/web/[0-9]{14}/', archivurl): return True
+    if archivurl[28:36] == archivdatum or archivurl[28:36] == abrufdatum or archivurl[28:36] == datum: return True
+    if archivdatum == None: return "Kein Archivierungsdatum gesetzt."
+    return "Falsches Archivierungsdatum gesetzt."
+
+class Problem(dict):
+    def __init__(self, titel: str|None=None, problemtyp: str|None=None, snippet: str|None=None, foundDate: str|None=None, dictionary: dict = {}):
+        self.titel: str      = titel      if titel      != None else dictionary.get('titel')
+        self.problemtyp: str = problemtyp if problemtyp != None else dictionary.get('problemtyp')
+        self.snippet: str    = snippet    if snippet    != None else dictionary.get('snippet')
+        self.foundDate: str  = foundDate  if foundDate  != None else dictionary.get('foundDate')
+        self.revision: int|None = dictionary.get('revision')
+    def __str__(self):
+        return self.titel + ': ' + self.problemtyp + ' ' + self.snippet
+    def __eq__(self, other):
+        if type(other) != Problem: return False
+        if self.titel != other.titel: return False
+        if self.problemtyp != other.problemtyp: return False
+        if self.snippet != other.snippet: return False
+        return True
+    def toDict(self):
+        return {'titel': self.titel, 'problemtyp': self.problemtyp, 'snippet': self.snippet, 'foundDate': self.foundDate, 'revision': self.revision}
+
+def loadAllProblems():
+    with io.open('./problems.json', encoding='utf8') as file:
+        allProblems: list[dict] = json.load(file)
+    return [Problem(dictionary=problem) for problem in allProblems]
+
+def dumpAllProblems(allProblems: list[Problem]):
+    with io.open('./problems.json', 'w', encoding='utf8') as file:
+        json.dump([problem.toDict() for problem in allProblems], file, ensure_ascii=False, indent=2)
+
+def checkPage(titel: str, content: str, todayString: str):
+    for template in wtp.parse(content).templates:
+        result = datesOk(template)
+        if True != result: 
+            yield Problem(titel, result, str(template), todayString)
+        result = archiveParamsOk(template)
+        if True != result: 
+            yield Problem(titel, result, str(template), todayString)
+
+def monitorRecentChanges():
+    allProblems = loadAllProblems()
+    stream = eventstreams.EventStreams(streams='recentchange')
+    site = pywikibot.Site('de', 'wikipedia')
+    stream.register_filter(type='edit', wiki='dewiki', namespace=0)
+    numberOfChanges = 0
+    numberOfProblems = 0
+    while True:
+        try:
+            change = next(stream)
+            numberOfChanges += 1
+            page = pywikibot.Page(site, 'University of Rwanda')#change['title'])
+            content = page.get()
+            for problem in checkPage(page.title(), content, getTodayString()):
+                if problem in allProblems: continue
+                numberOfProblems += 1
+                for rev in page.revisions(total=50):
+                    try:
+                        if rev['parentid'] == 0 or problem not in checkPage(page.title(), page.getOldVersion(rev['parentid']), getTodayString()): 
+                            problem.revision = rev['revid']
+                            break
+                    except Exception as e:
+                        e.add_note(f'failed while checking if problem already existed before revision {rev.get('revid')}')
+                        raise e
+                allProblems.append(problem)
+                print('Problem:', problem)
+                dumpAllProblems(allProblems)
+            if numberOfChanges >= 100:
+                print(f'Checked 100 changes and found {numberOfProblems} problems.')
+                numberOfProblems = 0
+                numberOfChanges = 0
+        except pywikibot.exceptions.IsRedirectPageError:
+            continue
+        except Exception as e:
+            e.add_note(f'failed while handling recent change {change.get('revision')}')
+            raise e
+
+def checkProblemList():
+    allProblems = loadAllProblems()
+    print(f'checking list of {len(allProblems)} problems ...')
+    site = pywikibot.Site('de', 'wikipedia')
+    index = 0
+    while index < len(allProblems):
+        problem = allProblems[index]
+        page = pywikibot.Page(site, problem.titel)
+        if problem in checkPage(problem.titel, page.get(), problem.foundDate): 
+            index += 1
+        else:
+            print('Problem in', problem.titel, 'abgearbeitet.')
+            del allProblems[index]
+    dumpAllProblems(allProblems)
+    
+def updateWikilist():
+    allProblems = loadAllProblems()
+    datum = None
+    titel = None
+    wikitext = ''
+    for problem in allProblems:
+        if datum != problem.foundDate:
+            wikitext += f'== {problem.foundDate} ==\n\n'
+        if titel != problem.titel or datum != problem.foundDate:
+            wikitext += '{{Überschriftensimulation|3|Text=[[' + problem.titel + ']]}}\n'
+        wikitext += f'{problem.problemtyp}{'' if problem.revision==None else f' ([[Spezial:Diff/{problem.revision}|Änderung]])'}\n'
+        wikitext += f'<pre>{problem.snippet}</pre>\n\n'
+        datum = problem.foundDate
+        titel = problem.titel
+    site = pywikibot.Site('de', 'wikipedia')
+    page = pywikibot.Page(site, 'Benutzer:DerIchBot/Wartungsliste')
+    page.text = wikitext
+    site.login()
+    page.save(botflag=True, minor=False, summary=(f'Bot: Aktualisiere Wartungsliste: {len(allProblems)} Einträge'))
+    site.logout()
+
+def run():
+    checkProblemList()
+    updateWikilist()
+    monitorRecentChanges()
+
+if __name__ == '__main__':
+    try:
+        run()
+    except KeyboardInterrupt:
+        print('Exception: KeyboardInterrupt')
+            
