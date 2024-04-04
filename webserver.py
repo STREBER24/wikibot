2bda5fbb9634308137b779e4fc46838abd5c4a28
STREBER24 <largot@gmx.de>
2024-04-04 17:07:26 +0000

    show number of saved problems and catch errors on webserver

--- webserver.py
+++ webserver.py
@@ -1,4 +1,5 @@
 import recentChanges
+import traceback
 import utils
 import flask
 
@@ -23,6 +24,16 @@ def aktualisiere_wartungsliste_check():
         return 'Wartungsliste erfolgreich geprüft und aktualisiert. Du wirst gleich weitergeleitet ...', {"Refresh": "4; url=https://de.wikipedia.org/wiki/Benutzer:DerIchBot/Wartungsliste"}
     else:
         return 'Aktualisierung verweigert, da letzte Aktualisierung mit Prüfung weniger als 15 Minuten her ist.'
+
+@app.route('/number-of-problems')
+def number_of_problems():
+    return f'{len(recentChanges.loadAllProblems())} Probleme im Cache'
+
+@app.errorhandler(500)
+def exception_handler(error):
+    app.logger.error(f'STATUS 500\n\n{traceback.format_exc()}')
+    utils.sendTelegram(f'{traceback.format_exc()}')
+    return "Unerwarteter interner Fehler aufgetreten. Der Betreiber wurde bereits benachrichtigt.", 500
 
 if __name__=='__main__': 
     app.run(host=HOST, port=PORT)
