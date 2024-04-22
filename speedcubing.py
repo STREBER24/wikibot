STREBER24/wikibot

commit 5b091822b0b0614fc7c2b3368d627b477845ffba

enforce types with mypy

speedcubing.py
@@ -27,7 +27,7 @@ disciplines = {'2x2x2': '2x2x2 Cube',
                'Clock': 'Clock'}
 
 def differentLinks(name: str):
-    links = dict()
+    links: dict[str, str] = dict()
     if links.get(name) != None:
         return links.get(name)
     if re.search(' \\(.+\\)$', name):
@@ -56,7 +56,7 @@ def editWiki(data: dict[str, tuple[list[dict[str, str]], list[dict[str, str]]]],
         print('skipped')
 
 def generatePage(data: dict[str, tuple[list[dict[str, str]], list[dict[str, str]]]], parser: typing.Callable[[list[dict[str, str]], str], str]):
-    return '<onlyinclude><includeonly><!--\n-->{{#switch: {{{2}}}<!--\n  -->| Single = {{#switch: {{{1}}}' + ''.join(['<!--\n    -->| '+i.ljust(16)+' = '+parser(data.get(disciplines.get(i))[0], i) for i in disciplines.keys()]) + '<!--\n    -->| #default         = ' + parseError('Parameter 1 ungültig!') + ' }}<!--\n  -->| Average = {{#switch: {{{1}}}' + ''.join(['<!--\n    -->| '+i.ljust(16)+' = '+parser(data.get(disciplines.get(i))[1], i) for i in disciplines.keys()]) + '<!--\n    -->| #default         = ' + parseError('Parameter 1 ungültig!') + ' }}<!--\n  -->| #default = ' + parseError('Parameter 2 ungültig!') + '<!--\n-->}}</includeonly></onlyinclude>\n\n{{Dokumentation}}'
+    return '<onlyinclude><includeonly><!--\n-->{{#switch: {{{2}}}<!--\n  -->| Single = {{#switch: {{{1}}}' + ''.join(['<!--\n    -->| '+i.ljust(16)+' = '+parser(data[disciplines[i]][0], i) for i in disciplines.keys()]) + '<!--\n    -->| #default         = ' + parseError('Parameter 1 ungültig!') + ' }}<!--\n  -->| Average = {{#switch: {{{1}}}' + ''.join(['<!--\n    -->| '+i.ljust(16)+' = '+parser(data[disciplines[i]][1], i) for i in disciplines.keys()]) + '<!--\n    -->| #default         = ' + parseError('Parameter 1 ungültig!') + ' }}<!--\n  -->| #default = ' + parseError('Parameter 2 ungültig!') + '<!--\n-->}}</includeonly></onlyinclude>\n\n{{Dokumentation}}'
 
 def parseSwitch(data: list[str], key: int):
     data = (['', '', '', ''] + data)[-4:]
@@ -65,7 +65,7 @@ def parseSwitch(data: list[str], key: int):
 def parseDates(data: list[dict[str, str]], discipline: str):
     if data == []: return parseError('Keine Daten für diese Parameterkombination.')
     months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-    dates = [i.get('date') for i in data]
+    dates = [i['date'] for i in data]
     dates = [utils.formatDate(i[4:6], months.index(i[0:3])+1, i[8:12]) for i in dates]
     return parseSwitch(dates, 3)
 
@@ -73,28 +73,28 @@ def parseNames(data: list[dict[str, str]], discipline: str):
     if data == []: return parseError('Keine Daten für diese Parameterkombination.')
     months = {'Jan': 'Januar', 'Feb': 'Februar', 'Mar': 'März', 'Apr': 'April', 'May': 'Mai', 'Jun': 'Juni',
               'Jul': 'Juli', 'Aug': 'August', 'Sep': 'September', 'Oct': 'Oktober', 'Nov': 'November', 'Dec': 'Dezember'}
-    names = [i.get('name') for i in data]
+    names = [i['name'] for i in data]
     links = ['[[' + (i if differentLinks(i)==None else differentLinks(i)+'|'+i) + ']]' for i in names]
     return '{{#if: {{#invoke:TemplUtl|faculty|{{{3|}}}}} |<!--\n      -->' + parseSwitch(links, 4) + '|<!--\n      -->' + parseSwitch(names, 4) + '}}'
 
 def parseTime(data: list[dict[str, str]], discipline: str):
     if data == []: return parseError('Keine Daten für diese Parameterkombination.')
     ergebnis = data[0].get('single') if data[0].get('single') != '' else data[0].get('average')
-    assert ergebnis != '' and ergebnis != None
+    assert isinstance(ergebnis, str) and ergebnis != ''
     einheit = 'Züge' if 'moves' in discipline else ('Minuten' if ':' in ergebnis else 'Sekunden')
-    ergebnis = ergebnis.replace('.',',').split(' ')
-    return ergebnis[0] + '<!--' + ' '*(8-len(ergebnis[0])) + '-->{{#if: {{#invoke:TemplUtl|faculty|{{{3|}}}}} | ' + ('&#32;in '+ergebnis[1] if len(ergebnis)>1 else '') + '&nbsp;' + einheit + '}}'
+    ergebnisList = ergebnis.replace('.',',').split(' ')
+    return ergebnis[0] + '<!--' + ' '*(8-len(ergebnisList[0])) + '-->{{#if: {{#invoke:TemplUtl|faculty|{{{3|}}}}} | ' + ('&#32;in '+ergebnisList[1] if len(ergebnisList)>1 else '') + '&nbsp;' + einheit + '}}'
 
 def parseEvents(data: list[dict[str, str]], discipline: str):
     if data == []: return parseError('Keine Daten für diese Parameterkombination.')
-    events = [i.get('competition') for i in data]
+    events = [i['competition'] for i in data]
     return parseSwitch(events, 3)
 
 def parseError(text: str):
     return f'<span class="error">{text}</span> [[Kategorie:Wikipedia:Vorlagenfehler/Speedcubing]]'
 
 def stripTag(tag: bs4.element.Tag | None):
-    if tag == None:
+    if not isinstance(tag, bs4.element.Tag):
         return ''
     return tag.text.strip()
 
@@ -104,15 +104,16 @@ def scrape():
     assert result.ok
     soup = bs4.BeautifulSoup(result.text, 'html.parser')
     body = soup.find(id='results-list')
+    assert isinstance(body, bs4.element.Tag)
     titles = body.find_all('h2')
     tables = body.find_all('div', {'class': 'table-responsive'})
     assert len(titles) == len(tables)
     data: dict[str, tuple[list[dict[str, str]], list[dict[str, str]]]] = dict()
     for title, table in zip(titles, tables):
-        title: bs4.element.Tag
-        table: bs4.element.Tag
+        assert isinstance(title, bs4.element.Tag)
+        assert isinstance(table, bs4.element.Tag)
         tbody = table.find('tbody')
-        assert tbody != None
+        assert isinstance(tbody, bs4.element.Tag)
         results: list[dict[str, str]] = [{'date': stripTag(line.find('td', {'class': 'date'})),
                                           'single': stripTag(line.find('td', {'class': 'single'})),
                                           'average': stripTag(line.find('td', {'class': 'average'})),
@@ -135,7 +136,7 @@ def run():
         oldData: dict[str, tuple] = json.loads(file.read())
     with io.open('speedcubing-data.json', 'w', encoding='utf8') as file:
         json.dump(newData, file, indent=2, ensure_ascii=False)
-    changedDisciplines = [i for i in disciplines.keys() if json.dumps(oldData.get(disciplines.get(i)), ensure_ascii=False) != json.dumps(newData.get(disciplines.get(i)), ensure_ascii=False)]
+    changedDisciplines = [i for i in disciplines.keys() if json.dumps(oldData.get(disciplines[i]), ensure_ascii=False) != json.dumps(newData.get(disciplines.get(i)), ensure_ascii=False)]
     changes = changedDisciplines != []
     if not changes:
         print('Keine Änderung an den Rohdaten.')

mypy.ini
@@ -1,0 +1,12 @@
+[mypy]
+check_untyped_defs = True
+exclude = user-config.py
+
+[mypy-pywikibot]
+ignore_missing_imports = True
+
+[mypy-pywikibot.comms]
+ignore_missing_imports = True
+
+[mypy-wikitextparser]
+ignore_missing_imports = True

recentChanges.py
@@ -11,7 +11,7 @@ import io
 def parseWeirdDateFormats(date: str|None):
     ''' Wandelt möglichts alle Datumsformate, die die Vorlage Internetquelle akzeptiert in Format YYYY-MM-DD um '''
     try:
-        if date == None: return None
+        if type(date) is not str: return None
         if re.match('^[0-9]{4}(-[0-9]{2}(-[0-9]{2})?)?$', date): 
             return date
         if re.match('^[0-9]{4}-[0-9]{2}-[0-9]$', date): 
@@ -67,10 +67,10 @@ def archiveParamsOk(template: wtp.Template) -> Literal[True] | str:
 
 class Problem(dict):
     def __init__(self, titel: str|None=None, problemtyp: str|None=None, snippet: str|None=None, foundDate: str|None=None, dictionary: dict = {}):
-        self.titel: str      = titel      if titel      != None else dictionary.get('titel')
-        self.problemtyp: str = problemtyp if problemtyp != None else dictionary.get('problemtyp')
-        self.snippet: str    = snippet    if snippet    != None else dictionary.get('snippet')
-        self.foundDate: str  = foundDate  if foundDate  != None else dictionary.get('foundDate')
+        if not isinstance(titel,      str): titel      = dictionary.get('titel');      assert isinstance(titel, str);      self.titel      = titel
+        if not isinstance(problemtyp, str): problemtyp = dictionary.get('problemtyp'); assert isinstance(problemtyp, str); self.problemtyp = problemtyp
+        if not isinstance(snippet,    str): snippet    = dictionary.get('snippet');    assert isinstance(snippet, str);    self.snippet    = snippet
+        if not isinstance(foundDate,  str): foundDate  = dictionary.get('foundDate');  assert isinstance(foundDate, str);  self.foundDate  = foundDate
         self.revision: int|None = dictionary.get('revision')
     def __str__(self):
         return self.titel + ': ' + self.problemtyp + ' ' + self.snippet

schools-bw.py
@@ -1,9 +1,9 @@
+from typing import Any
 import wikitextparser as wtp
 import pywikibot
 import requests
 import random
 import utils
-import time
 import json
 
 def getAllSchoolDischs(search: str|None='', multiplePages: bool=False) -> set[str]:
@@ -23,7 +23,7 @@ def getAllSchoolDischs(search: str|None='', multiplePages: bool=False) -> set[st
                 res.json().get('d')).get('Rows')
             assert len(rows) > 0
             for school in rows:
-                data.add(school.get('DISCH').strip("'"))
+                data.add(school['DISCH'].strip("'"))
             pageNumber += 1
             if not multiplePages: ok = False
         except AssertionError:
@@ -64,7 +64,7 @@ def addAllDischs():
             page.text = parsed
             page.save(botflag=True, minor=False, summary=(f'Bot: Ergänze Schulnummer (DISCH). Siehe km-bw.de/Schuladressdatenbank'))
 
-def addWikidataNumberClaim(repo: any, item: pywikibot.ItemPage, property: str, number: int, url: str, pointInTime: pywikibot.WbTime):
+def addWikidataNumberClaim(repo: Any, item: pywikibot.ItemPage, property: str, number: int, url: str, pointInTime: pywikibot.WbTime):
     if property in item.get()['claims']:
         print(f'skip update of {item.title()} because {property} is already set.')
         return

utils.py
@@ -1,5 +1,6 @@
 from pywikibot import pagegenerators as pg
 import wikitextparser as wtp
+from typing import Any
 import pywikibot
 import time
 import json
@@ -7,9 +8,13 @@ import bs4
 import io
 
 def getText(tag: bs4.Tag | str | None) -> str:
-    if tag == None: return ''
-    if type(tag) == bs4.Tag: tag = tag.text
-    return tag.strip().replace('  ', ' ')
+    if type(tag) is bs4.Tag: 
+        text = tag.text
+    elif type(tag) is str:
+        text = tag
+    else: 
+        return ''
+    return text.strip().replace('  ', ' ')
 
 def formatDate(day: str|int, month: str|int, year: str|int):
     months = ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember']
@@ -40,17 +45,18 @@ def findTemplateArg(template: wtp.Template, argName: str):
 def checkLastUpdate(key: str, minDelayDays: int):
     try:
         with io.open('last-updates.json', encoding='utf8') as file:
-            data: dict = json.load(file)
+            data: dict[str,int] = json.load(file)
     except:
         data = dict()
-    if data.get(key) != None and data.get(key) > time.time() - (minDelayDays*60*60*24):
+    lastUpdate = data.get(key)
+    if type(lastUpdate) is int and lastUpdate > time.time() - (minDelayDays*60*60*24):
         return False
     data[key] = int(time.time())
     with io.open('last-updates.json', 'w', encoding='utf8') as file:
         json.dump(data, file)
     return True
 
-def addWikidataSource(repo: any, claim: pywikibot.Claim,  url: str):
+def addWikidataSource(repo: Any, claim: pywikibot.Claim,  url: str):
     now = time.localtime()
     today = pywikibot.WbTime(year=now.tm_year, month=now.tm_mon, day=now.tm_mday)
     ref = pywikibot.Claim(repo, 'P854')

commit 9fdbaf250cb8fc95244f59d407e23eae8dee813a

fix date parsing in speedcubing ... bug introduced in 5b091822b0b

speedcubing.py
@@ -83,7 +83,7 @@ def parseTime(data: list[dict[str, str]], discipline: str):
     assert isinstance(ergebnis, str) and ergebnis != ''
     einheit = 'Züge' if 'moves' in discipline else ('Minuten' if ':' in ergebnis else 'Sekunden')
     ergebnisList = ergebnis.replace('.',',').split(' ')
-    return ergebnis[0] + '<!--' + ' '*(8-len(ergebnisList[0])) + '-->{{#if: {{#invoke:TemplUtl|faculty|{{{3|}}}}} | ' + ('&#32;in '+ergebnisList[1] if len(ergebnisList)>1 else '') + '&nbsp;' + einheit + '}}'
+    return ergebnisList[0] + '<!--' + ' '*(8-len(ergebnisList[0])) + '-->{{#if: {{#invoke:TemplUtl|faculty|{{{3|}}}}} | ' + ('&#32;in '+ergebnisList[1] if len(ergebnisList)>1 else '') + '&nbsp;' + einheit + '}}'
 
 def parseEvents(data: list[dict[str, str]], discipline: str):
     if data == []: return parseError('Keine Daten für diese Parameterkombination.')